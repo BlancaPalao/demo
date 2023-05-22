b0d7b912457a55ff00980444695d5120868f4d08
Blanca <blanca@example.com>
2023-05-22 16:49:11 +0200

    Cambios realizados el 22/05 para corregir la incidenciaq INC-9847

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Cambios de nuevo" );
+        System.out.println( "Cambios del día 22/05" );
     }
 }
